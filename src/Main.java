eb1553b38b099ae73ca92b43648d45e4d367bcdd
Chiko Hiraki <e205735@ie.u-ryukyu.ac.jp>
2020-12-22 17:35:13 +0900

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 class Main{
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch(NullPointerException e){
+            System.out.println("NullPointerException が発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }
