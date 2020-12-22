adce0ca91ceed3e0a6eade325c0204b091ff74e6
Chiko Hiraki <e205735@ie.u-ryukyu.ac.jp>
2020-12-22 17:53:37 +0900

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 class Main{
     public static void main(String[] args) {
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch(NullPointerException e){
-            System.out.println("NullPointerException が発生しました");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
